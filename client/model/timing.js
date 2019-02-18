f287a10af1415b0d29433f51a0010f442c8b1bf1
Jacek Wielemborek <d33tah@gmail.com>
2019-02-18 17:13:17 +0100

    set kSessionDuration = 30 * 60;

--- client/model/timing.js
+++ client/model/timing.js
@@ -31,7 +31,7 @@ import {assert} from '/lib/base';
 // card counts for the current session and the timestamp at which it began.
 // On each frame, we check whether the current session is over.
 
-const kSessionDuration = 12 * 60 * 60;
+const kSessionDuration = 30 * 60;
 
 const timing = new PersistentVar('timing');
 
