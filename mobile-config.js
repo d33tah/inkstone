5777e8a84250aa86063f0f49d70a9f967a1921b0
Jacek Wielemborek <d33tah@gmail.com>
2019-03-19 15:18:20 +0100

    Merge.

--- mobile-config.js
+++ mobile-config.js
@@ -2,6 +2,6 @@ App.accessRule('https://d33tah.github.io/inkstone/*');
 App.info({
   name: 'Inkstone',
   description: 'Learn to write Chinese characters.',
-  version: '0.1.4'
+  version: '0.1.5'
 });
 App.setPreference('orientation', 'portrait');
